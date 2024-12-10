f554161be6478c57244701d2816422ce10ca1645
Fiona Hopkins <hopkinsf@dnc.org>
2024-12-10 11:12:39 -0500

    Log out modes during build

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,5 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
+console.log("MODE FOR ROUTES:", import.meta.env.MODE)
+
 export default [index("routes/home.tsx")] satisfies RouteConfig;
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
+export default defineConfig(({ mode }) => {
+  console.log("MODE FOR VITE CONFIG", mode)
+
+  return {
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
+      },
     },
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
+    plugins: [reactRouter(), tsconfigPaths()],
+  }
 });
